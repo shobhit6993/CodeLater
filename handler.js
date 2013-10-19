bf340d950e7d259b55080aa50778881022aa7431
Shobhit Chaurasia <sc.shobhit@gmail.com>
2013-10-19 16:50:22 +0530

    Check todo list before enabling CodeLater button

--- handler.js
+++ handler.js
@@ -5,23 +5,36 @@ var DEBUG=true;
 	if(/http:\/\/www.codechef.com\/[^\/]*\/problems\/.*/.test(url) 
 		|| /http:\/\/www.codechef.com\/problems\/.*/.test(url)) 
 	{
+		
 		var buttons = $(".button-list");
 		var title= $("table.pagetitle-prob").find("td").eq(0).find("h1").text();
-		// if(DEBUG) console.log(title);
 		var id=$("table.pagetitle-prob").find("td").eq(0).find("p").text().split(" ")[2];
-		// if(DEBUG) console.log(id);
-		add = "<li > <a href='#' title='" + title + "' id='" + id + "' hreff='" + url + "' class='codeLater toDoAllowed' >CodeLater</a></li>"
-		// if(DEBUG) console.log(add);
+		add = "<li > <a href='#' title='" + title + "' id='" + id + "' hreff='" + url.replace('#','') + "' class='codeLater toDoAllowed' >CodeLater</a></li>"
 		$("table.pagetitle-prob").find("td").eq(0).attr("width", "40%");
 		buttons.append(add);
 
+		//check if prob already added. if yes, then strike the CodeLater button.
+		chrome.storage.sync.get( null, function (items)
+		{
+			for(k in items)
+			{
+				if(items[k]['href'].replace('#','')===url.replace('#',''))
+				{ 
+					console.log("shobhit"); 
+					console.log("mainak");
+					var info=$(".button-list").find("li").find("a.codeLater");
+					info.css("text-decoration","line-through");
+					info.removeClass("toDoAllowed");
+					return;
+				}
+			}
+		});
+
 		$(document).on("click", ".codeLater", function(event)
 		{
-			// console.log("sho");
 			var info=$(".button-list").find("li").find("a.codeLater");
 			if (info.hasClass("toDoAllowed")) 
 			{
-			// console.log("sho");
 				var obj={};
 				var items={};
 				items['id']=info.attr('id');
