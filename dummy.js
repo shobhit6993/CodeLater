c537ac27b011b580bec15615bee6e9cfe9a58956
Shobhit Chaurasia <sc.shobhit@gmail.com>
2013-10-19 12:44:37 +0530

    Initial Commit

---
+++ dummy.js
@@ -0,0 +1,21 @@
+(function(){
+	chrome.storage.sync.clear();
+	obj={};
+	var items={};
+	var item={};
+	items['id']="N1";
+	items['title']="Treasure Hunting";
+	items['href']="http://www.codechef.com/problems/N1";
+	obj[items['href']]=items;
+	console.log(items);
+	item['id']="SUPERPLN";
+	item['title']="SUper Plane";
+	item['href']="http://www.codechef.com/problems/SUPERPLN";
+	obj[item['href']]=item;
+	console.log(item);
+
+	chrome.storage.sync.set(obj);
+	chrome.storage.sync.get( null, function (it){
+		console.log(it);});
+
+})();
